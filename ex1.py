9b728692cb8818a75ab24e8fecc15eb64adc8562
mostpalonen <joonas.palonen98@gmail.com>
2022-03-09 20:04:36 +0200

    Implemented the first exercise

--- ex1.py
+++ ex1.py
@@ -2,32 +2,75 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import neighbors
+from sklearn.model_selection import GridSearchCV
+import csv
+from datetime import datetime
+
+
+# KAGGLE SUBMISSION TEAM NAME: mostpalonen
+
+
+def unpickle(file):
+    """
+    Helper function that loads a given file from path
+    """
+    with open(file, 'rb') as pickleFile:
+        data = pickle.load(pickleFile)
+    return data
 
 
 def loadData():
-    with open("data/teenmagi/training_x.dat", 'rb') as pickleFile:
-        x_tr = pickle.load(pickleFile)
+    """
+    Load input data and transform the data from size (8,8,3) to (8,8,1).
+    Transformation reduces the complexity because colorchannels are duplicant. 
+    After this the input data is transformed to one dimensional (64,1) vectors.
+    """
 
-    with open("data/teenmagi/validation_x.dat", 'rb') as pickeFile:
-        x_val = pickle.load(pickeFile)
+    x_tr = unpickle("data/teenmagi/training_x.dat")
+    x_val = unpickle("data/teenmagi/validation_x.dat")
+    y_tr = unpickle("data/teenmagi/training_y.dat")
 
-    with open("data/teenmagi/training_y.dat", 'rb') as pickeFile:
-        y_tr = pickle.load(pickeFile)
+    for i in range(len(x_tr)):
+        x_tr[i] = x_tr[i][:,:,1].flatten()
+
+    for i in range(len(x_val)):
+        x_val[i] = x_val[i][:,:,1].flatten()
 
     return x_tr, x_val, y_tr
 
-def main():
 
-    # Parameters
-    n_neighbours = 1
+def main():
+    print(f"\nExecution started at {datetime.now()}")
+
+    # Dictionary of all the values used for finding optimal value for n_neighbors
+    param_grid = {'n_neighbors': np.arange(3, 11)}
 
     # Load data
     x_tr, x_val, y_tr = loadData()
-    print(x_tr[1].shape)
+    print(f"Data loaded")
 
-    # Initialize nearest neighbour classifier and validate it
-    clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbours, algorithm='kd_tree')
-    clf.fit(x_tr, y_tr)
-    y_pred = clf.predict(x_val)
+    # Initialize nearest neighbour classifier
+    clf = neighbors.KNeighborsClassifier()
+
+    # Use gridsearch to test all values for n_neighbors
+    clf_gscv = GridSearchCV(clf, param_grid, cv=5, verbose=2)
+
+    # Fit model to data
+    clf_gscv.fit(x_tr, y_tr)
+    print(f"Fit done")
+
+    # Do predictions with the best performing estimator
+    y_pred = clf_gscv.best_estimator_.predict(x_val)
+    print(f"Predictions done")
+
+    # Write CSV of predictions
+    with open("data/teenmagi/predictions.csv", "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Id", "Class"])
+        i = 1
+        for item in y_pred:
+            writer.writerow([i, item])
+            i += 1
+    print(f"CSV written \n")
 
 main()
