5a7288f81b29c1f01adf0ce3626f46d1233be7f4
mostpalonen <joonas.palonen98@gmail.com>
2022-03-26 14:08:38 +0200

    Finalized ex3

--- ex3.py
+++ ex3.py
@@ -3,18 +3,23 @@ import numpy as np
 
 
 def main():
-    # Create signals (clean and noisy)
-    n = np.arange(500, 600)
+    # Create noisy signal and detectors
+    n = np.arange(100)
     y = np.concatenate((np.zeros(500), np.cos(2 * np.pi * 0.1 * n), np.zeros(300)))
+    exp_y = np.exp(-2*np.pi * 1j * 0.1 * n)
     y_n = y + np.sqrt(0.5) * np.random.randn(y.size)
+    
+    # Convolve signals with detector
+    det1 = np.convolve(y, y_n, 'same')
+    det1full = np.convolve(y, y_n, 'full')
+    det2 = np.convolve(exp_y, y_n, 'same')
 
-    det = np.convolve(y_n, np.flip(y), 'same')
+    # Plot detections
+    plot(y, y_n, det1full[500:1500], det1, det2)
 
-    plot(y, y_n, det)
 
-
-def plot(y, y_n, d):
-    fig, axs = plt.subplots(3, 1)
+def plot(y, y_n, d1, d1full, d2):
+    _, axs = plt.subplots(4, 1)
 
     axs[0].plot(y)
     axs[0].set_title('Noiseless Signal')
@@ -22,8 +27,12 @@ def plot(y, y_n, d):
     axs[1].plot(y_n)
     axs[1].set_title('Noisy Signal')
 
-    axs[2].plot(d)
-    axs[2].set_title('Detection Result')
+    axs[2].plot(d1)
+    axs[2].plot(d1full)
+    axs[2].set_title('Detection with cos. Blue = manually centered full convolution, Oragne = "same" convolution')
+
+    axs[3].plot(d2)
+    axs[3].set_title('Detection with exp')
 
     plt.show()
 
